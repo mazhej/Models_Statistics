823c2b98b3f2a94997b8285cb77bf95ce908dda5
Maziar <mazhej>
2020-01-21 11:12:54 -0800

    init

---
+++ Pytorch_Models_Statistics.py
@@ -0,0 +1,255 @@
+import argparse
+import os
+import random
+import shutil
+import time
+import warnings
+import json
+
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim
+import torch.utils.data
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+import torchvision.models as models
+
+from Stat_Hook import *
+
+#getting all the pytorch models name
+model_names = sorted(name for name in models.__dict__
+    if name.islower() and not name.startswith("__")
+    and callable(models.__dict__[name]))
+
+parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
+parser.add_argument('data', metavar='DIR',
+                    help='path to dataset')
+parser.add_argument('-a', '--arch', metavar='ARCH', default='resnet18',
+                    choices=model_names,
+                    help='model architecture: ' +
+                        ' | '.join(model_names) +
+                        ' (default: resnet18)')
+parser.add_argument('--epochs', default=90, type=int, metavar='N',
+                    help='number of total epochs to run')
+parser.add_argument('-b', '--batch-size', default=256, type=int,
+                    metavar='N',
+                    help='mini-batch size (default: 256), this is the total '
+                         'batch size of all GPUs on the current node when '
+                         'using Data Parallel or Distributed Data Parallel')
+parser.add_argument('--lr', '--learning-rate', default=0.1, type=float,
+                    metavar='LR', help='initial learning rate', dest='lr')
+parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
+                    help='momentum')
+parser.add_argument('--wd', '--weight-decay', default=1e-4, type=float,
+                    metavar='W', help='weight decay (default: 1e-4)',
+                    dest='weight_decay')
+parser.add_argument('--resume', default='', type=str, metavar='PATH',
+                    help='path to latest checkpoint (default: none)')
+parser.add_argument('-e', '--evaluate', dest='evaluate', action='store_true',
+                    help='evaluate model on validation set')
+parser.add_argument('--pretrained', dest='pretrained', action='store_true',
+                    help='use pre-trained model')
+parser.add_argument('--gpu', default=None, type=int,
+                    help='GPU id to use.')
+parser.add_argument('--seed', default=None, type=int,
+                    help='seed for initializing training. ')
+parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
+                    help='number of data loading workers (default: 4)')
+parser.add_argument('-p', '--print-freq', default=10, type=int,
+                    metavar='N', help='print frequency (default: 10)')
+
+best_acc1 = 0
+
+def main():
+    args = parser.parse_args()
+    ngpus_per_node = torch.cuda.device_count()
+    main_worker(args.gpu, ngpus_per_node, args)
+    
+def main_worker(gpu, ngpus_per_node, args):
+    global best_acc1
+    args.gpu = gpu
+    if args.gpu is not None:
+        print("Use GPU: {} for training".format(args.gpu))
+
+    # create model
+    if args.pretrained:
+        print("=> using pre-trained model '{}'".format(args.arch))
+        model = models.__dict__[args.arch](pretrained=True)
+    else:
+        print("=> creating model '{}'".format(args.arch))
+        model = models.__dict__[args.arch]()
+    model.cuda()
+    criterion = nn.CrossEntropyLoss().cuda(args.gpu)
+    optimizer = torch.optim.SGD(model.parameters(), args.lr,
+                                 momentum=args.momentum,
+                                 weight_decay=args.weight_decay)
+
+    cudnn.benchmark = True
+
+    # Data loading code(Validation)
+    valdir = os.path.join(args.data, 'val')
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
+    
+    val_loader = torch.utils.data.DataLoader(
+        datasets.ImageFolder(valdir, transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            normalize,
+        ])),
+        batch_size=args.batch_size, shuffle=False,
+        num_workers=args.workers, pin_memory=True)
+
+    if args.evaluate:
+        validate(val_loader, model, criterion, args)
+        return
+
+def validate(val_loader, model,criterion,args):
+    #register a hook on each module
+    hookF = {}
+    for name, module in model.named_modules():
+        hookF[name] = statistics(module)
+
+    start_fun = time.time()
+    # start_cuda = torch.cuda.Event(enable_timing=True)
+    # end_cuda = torch.cuda.Event(enable_timing=True)
+    batch_time = AverageMeter('Time', ':6.3f')
+    losses = AverageMeter('Loss', ':.4e')
+    top1 = AverageMeter('Acc@1', ':6.2f')
+    top5 = AverageMeter('Acc@5', ':6.2f')
+    progress = ProgressMeter(
+    len(val_loader),
+    [batch_time, losses, top1, top5],
+    prefix='Test: ')
+    # switch to evaluate mode
+    model.eval()
+    total_time = 0
+    total_measure = 0
+    total_elapsed = 0
+    with torch.no_grad():
+        end = time.time()
+        for i, (images, target) in enumerate(val_loader):
+           
+            #sending images to GPU
+            images = images.cuda()
+            target = target.cuda()
+    
+            # compute output
+            output = model(images)
+            
+            # Waits for everything to finish running
+            torch.cuda.synchronize()
+            
+            #loss
+            loss = criterion(output, target)
+            
+            # measure accuracy and record loss
+            acc1, acc5 = accuracy(output, target, topk=(1, 5))
+            losses.update(loss.item(), images.size(0))
+            top1.update(acc1[0], images.size(0))
+            top5.update(acc5[0], images.size(0))
+        
+            # measure elapsed time
+            start_elapsed = time.time()
+            batch_time.update(time.time() - end)
+            end = time.time()
+            end_elapsed = time.time() - start_elapsed
+            total_elapsed += end_elapsed
+            
+
+            if i % args.print_freq == 0:
+                progress.display(i)
+
+    #create a dictionaty to see stattistics of the model
+    stat = {}
+    for key, value in hookF.items():
+       
+
+
+        stat[key] = [{"min_input": float(value.min_input.cpu().numpy()), 
+         "max_input": float(value.max_input.cpu().numpy()), "min_output": float(value.min_output.cpu().numpy()),
+         "max_output": float(value.max_output.cpu().numpy()),
+          "avg_min_input": float(value.avg_min_input.cpu().numpy()),
+           "avg_max_input": float(value.avg_max_input.cpu().numpy()),
+            "avg_min_output": float(value.avg_min_output.cpu().numpy()),
+             "avg_max_output": float(value.avg_max_output.cpu().numpy())}]
+    #save the dictionary as a json file
+    with open('imagenet_statistics.json','w')as fp:
+       for k,v in stat.items():
+            json.dump(k,fp,indent = 0)
+            json.dump(v,fp,indent= 0)
+            
+    return top1.avg 
+    
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self, name, fmt=':f'):
+        self.name = name
+        self.fmt = fmt
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+    def __str__(self):
+        fmtstr = '{name} {val' + self.fmt + '} ({avg' + self.fmt + '})'
+        return fmtstr.format(**self.__dict__)
+
+
+class ProgressMeter(object):
+    def __init__(self, num_batches, meters, prefix=""):
+        self.batch_fmtstr = self._get_batch_fmtstr(num_batches)
+        self.meters = meters
+        self.prefix = prefix
+
+    def display(self, batch):
+        entries = [self.prefix + self.batch_fmtstr.format(batch)]
+        entries += [str(meter) for meter in self.meters]
+        print('\t'.join(entries))
+
+    def _get_batch_fmtstr(self, num_batches):
+        num_digits = len(str(num_batches // 1))
+        fmt = '{:' + str(num_digits) + 'd}'
+        return '[' + fmt + '/' + fmt.format(num_batches) + ']'
+
+def adjust_learning_rate(optimizer, epoch, args):
+    """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
+    lr = args.lr * (0.1 ** (epoch // 30))
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the accuracy over the k top predictions for the specified values of k"""
+    with torch.no_grad():
+        maxk = max(topk)
+        batch_size = target.size(0)
+
+        _, pred = output.topk(maxk, 1, True, True)
+        pred = pred.t()
+        correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+        res = []
+        for k in topk:
+            correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
+            res.append(correct_k.mul_(100.0 / batch_size))
+        return res
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
---
+++ Stat_Hook.py
@@ -0,0 +1,47 @@
+import torch
+import numpy as np
+
+class statistics():
+    def __init__(self, module):
+        self.hook = module.register_forward_hook(self.hook_fn)
+        self.min_input = np.inf
+        self.max_input = - np.inf
+        self.min_output = np.inf
+        self.max_output = -np.inf
+        self.sum_min_input = 0
+        self.sum_max_input = 0
+        self.sum_min_output = 0
+        self.sum_max_output = 0
+        self.avg_min_input = 0
+        self.avg_max_input = 0 
+        self.avg_min_output = 0
+        self.avg_max_output = 0 
+        self.count = 0
+        
+    def hook_fn(self, module, input, output):
+        
+        if torch.min(input[0]) < self.min_input:
+            self.min_input = torch.min(input[0])
+
+        if torch.max(input[0]) > self.max_input:
+            self.max_input = torch.max(input[0])
+
+        if torch.min(output) < self.min_output:
+            self.min_output = torch.min(output)
+        if torch.max(output) > self.max_output:
+            self.max_output = torch.max(output)
+        for b in range(input[0].shape[0]):
+            self.sum_min_input += torch.min(input[0][b])
+            self.sum_max_input += torch.max(input[0][b])
+            self.sum_min_output += torch.min(output[b])
+            self.sum_max_output += torch.max(output[b])
+            self.count += 1
+        self.avg_min_input = self.sum_min_input / self.count
+        self.avg_max_input =  self.sum_max_input / self.count
+        self.avg_min_output = self.sum_min_output / self.count
+        self.avg_max_output = self.sum_max_output / self.count
+
+        
+
+    def close(self):
+        self.hook.remove()
